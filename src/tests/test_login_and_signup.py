3fce01abb1822c09813d650a0dca212c4f9485bc
VimalAnnamalai <iamvimalannamalai@gmail.com>
2024-08-13 03:27:30 +0530

    Added basic health check test

---
+++ src/tests/test_login_and_signup.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from ..app import app
+
+client = TestClient(app, base_url="http://myapp.com:8080")
+
+
+def test_health_check():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
